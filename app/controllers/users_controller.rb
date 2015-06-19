bcea3b7e15794f36d7b2de799482cbd8c4fb9958
Weih Li <wli@castlighthealth.com>
2015-06-19 11:43:44 -0700

    modify users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash.now[:notice] = [ 'Successfully Save!' ]
+    else
+      flash.now[:errors] = current_user.error.full_messages
+    end
+
+    render :edit
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
