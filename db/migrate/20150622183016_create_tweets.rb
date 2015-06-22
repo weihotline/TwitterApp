c0abe5c7f3c5822c1871292f98054b74e3706cff
Weih Li <wli@castlighthealth.com>
2015-06-22 11:36:17 -0700

    migrate tweets

--- db/migrate/20150622183016_create_tweets.rb
+++ db/migrate/20150622183016_create_tweets.rb
@@ -1,8 +1,12 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+      t.string :content, null: false
+      t.integer :author_id, null: false
 
       t.timestamps
     end
+
+    add_index :tweets, :author_id
   end
 end
